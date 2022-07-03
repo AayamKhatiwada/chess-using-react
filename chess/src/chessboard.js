d6a1839323fa9f8ae942b900387f68b9d6cd712c
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-07-03 16:01:32 +0545

    Update chessboard.js

--- chess/src/chessboard.js
+++ chess/src/chessboard.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import './chessboard.css'
 
-const horin = ['a','b','c','d','e','f','g','h']
-const vartical = ['1','2','3','4','5','6','7','8']
+const vartical = ['a','b','c','d','e','f','g','h']
+const horin = ['1','2','3','4','5','6','7','8']
 
 export default function Chessboard(){
     let board = [];
-    for (let i = 0; i < horin.length; i++) {
-        for (let j = 0; j < vartical.length; j++) {
-            board.push(<span>{horin[i]}{vartical[j]}</span>)
+    for (let i = vartical.length-1; i >=0; i--) {
+        for (let j = 0; j < horin.length; j++) {
+            const num = i +j + 2;
+            if (num % 2 === 0) {
+                board.push(<div className='whitetiles'>{vartical[j]}{horin[i]}</div>)
+            }else{
+                board.push(<div className='blacktiles'>{vartical[j]}{horin[i]}</div>)
+            }
         }
     }
 
