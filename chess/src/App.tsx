28ddfff7db5cac78087a1a2fce84d23e934c464c
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-07-05 20:00:31 +0545

    changing files

--- chess/src/App.tsx
+++ chess/src/App.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Chessboard from './chessboard';
 
 function App() {
   return (
     <div className="App">
-      <Chessboard/>
+
     </div>
   );
 }
