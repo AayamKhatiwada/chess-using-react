446d91a50b9c53419f1220c06bb4d19aeb80f1f1
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-07-06 17:24:32 +0545

    Update App.tsx

--- chess/src/App.tsx
+++ chess/src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Chessboard from './chessboard';
 
 function App() {
   return (
     <div className="App">
-
+      <Chessboard/>
     </div>
   );
 }
