2b87e11f9578ab45e06ffbc5d6cd8efb92a1007c
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-07-07 16:06:54 +0545

    Update chessboard.tsx

--- chess/src/chessboard.tsx
+++ chess/src/chessboard.tsx
@@ -1,21 +1,54 @@
 import './chessboard.css';
-import React from 'react';
+import Tiles from './tiles';
 
 const horin = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 const vertical = ['1', '2', '3', '4', '5', '6', '7', '8']
+
+interface Piece {
+    image: string;
+    horin: number;
+    vertical: number;
+}
+
+const pieces: Piece[] = [];
+
+for (let p = 0; p < 2; p++) {
+    const type = p === 0 ? "b" :"w";
+    const y = p === 0 ? 7 : 0;
+
+    pieces.push({image: `items/knight_${type}.png`, horin: 1 ,vertical: y})
+    pieces.push({image: `items/knight_${type}.png`, horin: 5 ,vertical: y})
+    pieces.push({image: `items/rook_${type}.png`, horin: 0 ,vertical: y})
+    pieces.push({image: `items/rook_${type}.png`, horin: 7 ,vertical: y})
+    pieces.push({image: `items/bishop_${type}.png`, horin: 2 ,vertical: y})
+    pieces.push({image: `items/bishop_${type}.png`, horin: 6 ,vertical: y})
+    pieces.push({image: `items/king_${type}.png`, horin: 4 ,vertical: y})
+    pieces.push({image: `items/queen_${type}.png`, horin: 3 ,vertical: y})
+}
+
+for (let i = 0; i < 8; i++) {
+    pieces.push({image: "items/pawn_w.png", horin: i ,vertical: 1})
+}
+
+for (let i = 0; i < 8; i++) {
+    pieces.push({image: "items/pawn_b.png", horin: i ,vertical: 6})
+}
+
 
 export default function Chessboard(){
     let board = [];
     for (let i = vertical.length-1; i >=0 ; i--) {
         for (let j = 0; j < horin.length; j++) {
             let num = i+j+2;
+            let image = '';
 
-            if (num % 2 === 0) {
-                board.push(<div className='blacktiles'></div>)
-            }else{
-                board.push(<div className='whitetiles'></div>)
-            }
-            
+            pieces.forEach(p =>{
+                if (p.horin === j && p.vertical === i) {
+                    image = p.image
+                }
+            })
+
+            board.push(<Tiles number={num} image={image}/>)        
         }
     }
 
