f33e697e64b8be2fb96366cf7863711be1d7ddcd
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-07-07 16:07:01 +0545

    Create tiles.tsx

---
+++ chess/src/tiles.tsx
@@ -0,0 +1,14 @@
+import './tiles.css';
+
+interface Props{
+    number: number;
+    image?: string;
+}
+
+export default function Tiles({number, image}: Props){
+    if (number % 2 === 0) {
+        return <div className='whitetiles'><img src={image} alt=''/></div>
+    }else{
+        return <div className='blacktiles'><img src={image} alt='' /></div>
+    }
+}
