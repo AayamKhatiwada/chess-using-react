3d77a457cce714e9841793cb90f4013a180e73a4
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-07-08 18:21:52 +0545

    Update tiles.tsx

--- chess/src/tiles.tsx
+++ chess/src/tiles.tsx
@@ -8,8 +8,12 @@ interface Props{
 
 export default function Tiles({number, image}: Props){
     if (number % 2 === 0) {
-        return <div className='whitetiles'><img src={image} alt=''/></div>
+        return <div className='whitetiles'>
+            <div className='pieces' style={{backgroundImage: `url(${image})`}}></div>
+        </div>
     }else{
-        return <div className='blacktiles'><img src={image} alt='' /></div>
+        return <div className='blacktiles'>
+            <div className='pieces' style={{backgroundImage: `url(${image})`}}></div>
+        </div>
     }
 }
